62d548b4af015a8e063fcf58a9d50f4f379fe448
i-cant-code-on-unity <notengonideasupongo@gmail.com>
2025-12-20 12:34:41 +0100

    add animacion según velocidad

--- modules/player/player.gd
+++ modules/player/player.gd
@@ -4,9 +4,15 @@ class_name Player
 @export var speed : float = 100.00
 
 func _ready() -> void:
-	sprite_player.play("default")
+	pass
 	
 func _physics_process(delta: float) -> void:
 	var direction : Vector2 = Input.get_vector("left","right","up","down").normalized()
 	self.velocity = direction * speed
 	move_and_slide()
+
+func _process(delta: float) -> void:
+	if velocity == Vector2.ZERO:
+		sprite_player.stop()
+	else:
+		sprite_player.play("walk")
