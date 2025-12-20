0fe99fbb78ed049421ee1dece86c66fe2646fa73
i-cant-code-on-unity <notengonideasupongo@gmail.com>
2025-12-20 13:55:31 +0100

    Sword programed. Sword and enemy instace

--- modules/sword/sword.gd
+++ modules/sword/sword.gd
@@ -1,5 +1,9 @@
 extends Area2D
 class_name Sword
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
-func _process(delta: float) -> void:
-	pass
+func _on_timer_timeout() -> void:
+	var bodies = self.get_overlapping_bodies()
+	for body in bodies:
+		if body.has_method("take_damage"):
+			body.take_damage(2)
