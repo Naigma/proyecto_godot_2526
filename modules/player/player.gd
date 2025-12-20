eeb87f4a73d2b09f092bda56e0bcc2a16176636b
i-cant-code-on-unity <notengonideasupongo@gmail.com>
2025-12-20 13:33:37 +0100

    Add npc's interaction to player

--- modules/player/player.gd
+++ modules/player/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 class_name Player
 @onready var sprite_player: AnimatedSprite2D = $SpritePlayer
 @export var speed : float = 100.00
+@onready var interaction_area: Area2D = $InteractionArea
 
 func _ready() -> void:
 	pass
@@ -16,3 +17,9 @@ func _process(delta: float) -> void:
 		sprite_player.stop()
 	else:
 		sprite_player.play("walk")
+		
+	if Input.is_action_just_pressed("interact"):
+		var bodies = interaction_area.get_overlapping_bodies()
+		for body in bodies:
+			if body.has_method("interact"):
+				body.interact()
