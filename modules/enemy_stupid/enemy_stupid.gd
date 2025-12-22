51ee04c4822c79630718c7b1934f85a1aa27f495
Toland <notengonideasupongo@gmail.com>
2025-12-22 12:56:52 +0100

    Add damage animation

--- modules/enemy_stupid/enemy_stupid.gd
+++ modules/enemy_stupid/enemy_stupid.gd
@@ -2,12 +2,16 @@ extends CharacterBody2D
 class_name EnemyStupid
 
 @export var health : int = 20
+@onready var animator: AnimatedSprite2D = $Sprite2D
 
 func _process(delta: float) -> void:
 	pass
 	
 func take_damage(damage: int):
 	health = health - damage
+	animator.modulate = Color.INDIAN_RED
 	if health < 1:
 		queue_free()
+	await get_tree().create_timer(0.1).timeout
+	animator.modulate = Color.WHITE
 	 
