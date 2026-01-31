89b9a35e5d022075b43007b0e4e06f29faf3e736
uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu <mariosoutofernandez@gmail.com>
2026-01-31 13:50:48 +0100

    Health bar xd

--- modules/player/player.gd
+++ modules/player/player.gd
@@ -5,11 +5,24 @@ class_name Player
 @export var speed : float = 100.00
 @onready var interaction_area: Area2D = $InteractionArea
 @onready var weapon_pivot: Node2D = $WeaponPivot
-@export var health: int = 6
+@export var max_health: int = 6
+var health = max_health
 
 func _ready() -> void:
-	pass
-	
+	set_health_label()
+	$HealthBar.max_value=max_health
+	set_health_bar()
+
+func set_health_label() -> void:
+	$HealthLabel.text =  "Health: %s" % health
+
+func set_health_bar() -> void:
+	$HealthBar.value = health
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		take_damage(1)
+
 func _physics_process(delta: float) -> void:
 	var direction : Vector2 = Input.get_vector("left","right","up","down").normalized()
 	self.velocity = direction * speed
@@ -34,5 +47,9 @@ func take_damage(damage):
 	sprite_player.modulate = Color.INDIAN_RED
 	if health <= 0:
 		queue_free()
+		
+	set_health_label()
+	set_health_bar()
+	
 	await get_tree().create_timer(0.1).timeout
 	sprite_player.modulate = Color.WHITE
