535ba5868768b398d9084509241d6c443b549d82
Toland <notengonideasupongo@gmail.com>
2026-02-28 12:50:53 +0100

    Changed health bar colours

--- modules/player/player.gd
+++ modules/player/player.gd
@@ -9,15 +9,19 @@ class_name Player
 var health = max_health
 
 func _ready() -> void:
-	set_health_label()
 	$HealthBar.max_value=max_health
 	set_health_bar()
 
-func set_health_label() -> void:
-	$HealthLabel.text =  "Health: %s" % health
-
 func set_health_bar() -> void:
+	var ratio = float(health) / float(max_health)
 	$HealthBar.value = health
+	
+	if ratio > 0.6:
+		$HealthBar.modulate = Color.GREEN
+	elif ratio > 0.3:
+		$HealthBar.modulate = Color.ORANGE
+	else:
+		$HealthBar.modulate = Color.RED
 
 func _physics_process(delta: float) -> void:
 	var direction : Vector2 = Input.get_vector("left","right","up","down").normalized()
@@ -49,7 +53,6 @@ func take_damage(damage):
 	if health <= 0:
 		hide()
 		
-	set_health_label()
 	set_health_bar()
 	
 	await get_tree().create_timer(0.1).timeout
