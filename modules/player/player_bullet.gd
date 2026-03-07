60f518dc22ff1fc685210f5622442ca7489667ee
Toland <notengonideasupongo@gmail.com>
2026-03-07 12:48:23 +0100

    Add player_bullet decay_time

--- modules/player/player_bullet.gd
+++ modules/player/player_bullet.gd
@@ -4,6 +4,11 @@ class_name PlayerBullet
 @export var damage : int = 1
 @export var speed : float = 400
 var direction : Vector2 = Vector2.ZERO
+@export var decay_time : float = 1
+
+func _ready() -> void:
+	await get_tree().create_timer(decay_time).timeout
+	self.queue_free()
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is not Player and body.has_method("take_damage"):
