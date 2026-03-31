2aa04941b1a5c0612b52f78db5bd9e7eaa049d93
uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu <mariosoutofernandez@gmail.com>
2026-03-31 13:50:16 +0200

    ok

--- modules/currency/currency.gd
+++ modules/currency/currency.gd
@@ -1,10 +1,16 @@
 extends Node
 signal coins_updated(coins : int)
-var coins : int = 7
+var coins : int = 0
 
 func increment_coins(amount : int):
 	coins += amount
 	coins_updated.emit(coins)
 	print(coins)
-	
-	
+
+signal eggs_updated(eggs : int)
+var eggs : int = 0
+
+func increment_eggs(amount : int):
+	eggs += amount
+	eggs_updated.emit(eggs)
+	print(eggs)
--- modules/ui/ui.gd
+++ modules/ui/ui.gd
@@ -2,11 +2,17 @@ extends CanvasLayer
 class_name UI
 
 @onready var coins_label: Label = $CoinsLabel
+@onready var eggs_label: Label = $EggsLabel
 
 func _ready() -> void:
+	Eggs.eggs_updated.connect(update_egg_label)
+	update_egg_label(Eggs.eggs)
 	Currency.coins_updated.connect(update_coin_label)
 	update_coin_label(Currency.coins)
 	
+func update_egg_label(eggs : int):
+	eggs_label.text = "eggs: " + str(eggs)
+	
+
 func update_coin_label(coins : int):
 	coins_label.text = "coins: " + str(coins)
-	
