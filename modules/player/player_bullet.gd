e7549d57f426c31e0b82013fce3cc1d2f044917e
uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu <mariosoutofernandez@gmail.com>
2026-04-11 12:23:13 +0200

    Player tulet

--- modules/player/player_bullet.gd
+++ modules/player/player_bullet.gd
@@ -13,6 +13,7 @@ func _ready() -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body is not Player and body.has_method("take_damage"):
 		body.take_damage(damage)
+		self.queue_free()
 
 func _physics_process(delta: float) -> void:
 	position += direction * speed * delta
