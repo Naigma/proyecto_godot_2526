a98ee8edac41d11d4254c68e5ff388ebf93c5c86
Toland <notengonideasupongo@gmail.com>
2026-04-11 12:25:41 +0200

    Add breakable's type of pick_up

--- modules/breakable/breakable.gd
+++ modules/breakable/breakable.gd
@@ -2,16 +2,17 @@ extends StaticBody2D
 class_name Breakable
 var health : int = 10
 @onready var animator: AnimatedSprite2D = $AnimatedSprite2D
-@export var pick_up_scene : PackedScene 
+@export var pick_up_scene : PackedScene
+@export var pick_up_type : String
 
 func take_damage(damage: int):
 	health = health - damage
 	animator.modulate = Color.INDIAN_RED
 	if health < 1:
 		var instance = pick_up_scene.instantiate() as PickUp
-		instance.pick_type = "coin"
+		instance.pick_type = pick_up_type
 		instance.global_position = self.global_position
-		
+		# TODO: Aquí el tween
 		# TODO: Eliminar get parent
 		get_parent().add_child(instance)
 		
